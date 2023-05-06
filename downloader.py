eb154c37a17b9680f2e03bb7b34f2210c9b60d49
Sebastian Ptz <sebastian.ptz@outlook.com>
2023-05-06 09:36:54 +0200

    update script and git ignore

--- downloader.py
+++ downloader.py
@@ -31,10 +31,10 @@ del(curr)
 
 latitude_precision = 2 # Define precise a point is made
 latitude_count = int(1 + 180 / latitude_precision)
-max_longitude_count = latitude_count - 1
+max_longitude_count = latitude_count - 1 # max num. of points on one hemisphere
 
 # Saving data by using less point on poles where latitude and longitudes are closer to each other
-number_of_points_per_lat = [max(1, math.ceil(math.sin(math.pi * lon_index / max_longitude_count) * max_longitude_count)) for lon_index in range(latitude_count)]
+number_of_points_per_lat = [max(1, math.ceil(math.sin(math.pi * lon_index / max_longitude_count) * max_longitude_count)*2) for lon_index in range(latitude_count)]
 
 # List of Geo-Coordination
 point_lists = [[((lat_index * latitude_precision) - 90, (360 / numberOfPoints) * lon_index - 180) for lon_index in range(numberOfPoints) ] for lat_index, numberOfPoints in enumerate(number_of_points_per_lat)]
@@ -45,11 +45,18 @@ invalid_points = set()
 
 #os.mkdir("data")
 with open("downloader.log", "w") as log_file:
-  
   for week_index, (start_time, end_time) in enumerate(weeks): # enumerate weeks of month
     print(f"Starting download for week: {week_index + 1} of {len(weeks)}")
-    os.mkdir(f"data/{start_time}") # mkdir with unix timestamp for the start of a week
+    if not os.path.isdir(f"data/{start_time}"):
+      os.mkdir(f"data/{start_time}") # mkdir with unix timestamp for the start of a week
+    else:
+      print(f"The direcory {start_time} already exists")
+      
     for point_index, (lat, lon) in enumerate(points): # enemurate geo-points
+      file_name = f"data/{start_time}/{lat}_{lon}.json"
+      if os.path.isfile(file_name):
+        print(f"The file {file_name} already exists")
+        continue
       if (lat, lon) in invalid_points:
         continue
       url = f"https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&start={start_time}&cnt={datapoints_per_week}&appid={api_key}&units=metric"
